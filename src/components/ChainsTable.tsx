cf47cdf3513a9fb26578f8061e7e8173d9041b59
ameliawalcek <ameliawalcek@gmail.com>
2023-08-01 17:26:06 +0100

    cleaned project and added dependencies

---
+++ src/components/ChainsTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChainsTable: React.FC = () => {
+  return (
+    <div>Chains table</div>
+  )
+}
+
+export default ChainsTable
---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+const Loader: React.FC = () => {
+  return (
+    <div>Loader</div>
+  )
+}
+
+export default Loader
